e715a133cd1b2d387fa929bd96ecf8667e244759
Brian Nelson <brian@treegecko.com>
2014-09-16 22:08:26 -0600

    Work on build script and config file loading

--- source/SidServerDataProcessor.py
+++ source/SidServerDataProcessor.py
@@ -3,7 +3,7 @@ __author__ = 'bnelson'
 from SIDServer.Utilities import ConfigUtility
 from SIDServer.Controllers import SendToSidWatchServerController
 
-config = ConfigUtility.load('D:\SourceCode\Other\SidWatch\pySidServerDataProcessor\Config\sidwatch.cfg')
+config = ConfigUtility.load('./Config/sidwatch.cfg')
 
 controller = SendToSidWatchServerController(config)
 controller.start()
